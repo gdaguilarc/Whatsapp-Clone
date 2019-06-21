90cc5d9adf18990f5781ab3a4a57be870721c96c
gdaguilarc <gd.aguilarc@gmail.com>
2019-06-21 10:22:22 -0500

    Cache service with new subscriptions

--- client/src/services/cache.service.ts
+++ client/src/services/cache.service.ts
@@ -3,7 +3,14 @@ import { defaultDataIdFromObject } from 'apollo-cache-inmemory';
 import { ApolloClient } from 'apollo-client';
 import * as fragments from '../graphql/fragments';
 import * as queries from '../graphql/queries';
-import { MessageFragment, useMessageAddedSubscription } from '../graphql/types';
+
+import {
+  MessageFragment,
+  ChatFragment,
+  useMessageAddedSubscription,
+  useChatAddedSubscription,
+  useChatRemovedSubscription,
+} from '../graphql/types';
 
 type Client = ApolloClient<any> | DataProxy;
 
@@ -15,11 +22,30 @@ export const useCacheService = () => {
       }
     },
   });
+
+  useChatRemovedSubscription({
+    onSubscriptionData: ({ client, subscriptionData: { data } }) => {
+      if (data) {
+        eraseChat(client, data.chatRemoved);
+      }
+    },
+  });
+
+  useChatAddedSubscription({
+    onSubscriptionData: ({ client, subscriptionData: { data } }) => {
+      if (data) {
+        writeChat(client, data.chatAdded);
+      }
+    },
+  });
 };
+
 export const writeMessage = (client: Client, message: MessageFragment) => {
   type FullChat = { [key: string]: any };
   let fullChat;
+
   const chatIdFromStore = defaultDataIdFromObject(message.chat);
+
   if (chatIdFromStore === null) {
     return;
   }
@@ -32,17 +58,107 @@ export const writeMessage = (client: Client, message: MessageFragment) => {
   } catch (e) {
     return;
   }
+
   if (fullChat === null || fullChat.messages === null) {
     return;
   }
   if (fullChat.messages.some((m: any) => m.id === message.id)) return;
+
   fullChat.messages.push(message);
   fullChat.lastMessage = message;
+
   client.writeFragment({
     id: chatIdFromStore,
     fragment: fragments.fullChat,
     fragmentName: 'FullChat',
     data: fullChat,
+  });
+
+  let data;
+  try {
+    data = client.readQuery({
+      query: queries.chats,
+    });
+  } catch (e) {
+    return;
+  }
+
+  if (!data || data === null) {
+    return null;
+  }
+  if (!data.chats || data.chats === undefined) {
+    return null;
+  }
+  const chats = data.chats;
+
+  const chatIndex = chats.findIndex((c: any) => {
+    if (message === null || message.chat === null) return -1;
+    return c.id === message.chat.id;
+  });
+  if (chatIndex === -1) return;
+  const chatWhereAdded = chats[chatIndex];
+
+  // The chat will appear at the top of the ChatsList component
+  chats.splice(chatIndex, 1);
+  chats.unshift(chatWhereAdded);
+
+  client.writeQuery({
+    query: queries.chats,
+    data: { chats: chats },
+  });
+};
+
+export const writeChat = (client: Client, chat: ChatFragment) => {
+  const chatId = defaultDataIdFromObject(chat);
+  if (chatId === null) {
+    return;
+  }
+
+  client.writeFragment({
+    id: chatId,
+    fragment: fragments.chat,
+    fragmentName: 'Chat',
+    data: chat,
+  });
+
+  let data;
+  try {
+    data = client.readQuery({
+      query: queries.chats,
+    });
+  } catch (e) {
+    return;
+  }
+
+  if (!data) return;
+
+  const chats = data.chats;
+
+  if (!chats) return;
+  if (chats.some((c: any) => c.id === chat.id)) return;
+
+  chats.unshift(chat);
+
+  client.writeQuery({
+    query: queries.chats,
+    data: { chats },
+  });
+};
+
+export const eraseChat = (client: Client, chatId: string) => {
+  const chatType = {
+    __typename: 'Chat',
+    id: chatId,
+  };
+  const chatIdFromObject = defaultDataIdFromObject(chatType);
+  if (chatIdFromObject === null) {
+    return;
+  }
+  client.writeFragment({
+    id: chatIdFromObject,
+    fragment: fragments.fullChat,
+    fragmentName: 'FullChat',
+    data: null,
   });
   let data;
   try {
@@ -52,22 +168,13 @@ export const writeMessage = (client: Client, message: MessageFragment) => {
   } catch (e) {
     return;
   }
-  if (!data || data === null) {
-    return null;
-  }
-  if (!data.chats || data.chats === undefined) {
-    return null;
-  }
+  if (!data || !data.chats) return;
   const chats = data.chats;
-  const chatIndex = chats.findIndex((c: any) => {
-    if (message === null || message.chat === null) return -1;
-    return c.id === message.chat.id;
-  });
+  if (!chats) return;
+  const chatIndex = chats.findIndex((c: any) => c.id === chatId);
   if (chatIndex === -1) return;
-  const chatWhereAdded = chats[chatIndex];
   // The chat will appear at the top of the ChatsList component
   chats.splice(chatIndex, 1);
-  chats.unshift(chatWhereAdded);
   client.writeQuery({
     query: queries.chats,
     data: { chats: chats },
