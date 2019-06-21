23ebb067b83e2694ea21e0089a6ec52d1c424a7f
gdaguilarc <gd.aguilarc@gmail.com>
2019-06-21 10:23:23 -0500

    re-run of graph codegen

--- client/src/graphql/types.tsx
+++ client/src/graphql/types.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import gql from 'graphql-tag';
-import * as ReactApolloHooks from 'react-apollo-hooks';
 import * as ReactApollo from 'react-apollo';
+import * as ReactApolloHooks from 'react-apollo-hooks';
 export type Maybe<T> = T | null;
 export type MaybePromise<T> = Promise<T> | T;
 /** All built-in and custom scalars, mapped to their actual values */
@@ -38,6 +38,8 @@ export type Message = {
 export type Mutation = {
   __typename?: 'Mutation';
   addMessage?: Maybe<Message>;
+  addChat?: Maybe<Chat>;
+  removeChat?: Maybe<Scalars['ID']>;
 };
 
 export type MutationAddMessageArgs = {
@@ -45,10 +47,19 @@ export type MutationAddMessageArgs = {
   content: Scalars['String'];
 };
 
+export type MutationAddChatArgs = {
+  recipientId: Scalars['ID'];
+};
+
+export type MutationRemoveChatArgs = {
+  chatId: Scalars['ID'];
+};
+
 export type Query = {
   __typename?: 'Query';
   chats: Array<Chat>;
   chat?: Maybe<Chat>;
+  users: Array<User>;
 };
 
 export type QueryChatArgs = {
@@ -58,6 +69,8 @@ export type QueryChatArgs = {
 export type Subscription = {
   __typename?: 'Subscription';
   messageAdded: Message;
+  chatAdded: Chat;
+  chatRemoved: Scalars['ID'];
 };
 
 export type User = {
@@ -66,6 +79,23 @@ export type User = {
   name: Scalars['String'];
   picture?: Maybe<Scalars['String']>;
 };
+export type AddChatMutationVariables = {
+  recipientId: Scalars['ID'];
+};
+
+export type AddChatMutation = { __typename?: 'Mutation' } & {
+  addChat: Maybe<{ __typename?: 'Chat' } & ChatFragment>;
+};
+
+export type RemoveChatMutationVariables = {
+  chatId: Scalars['ID'];
+};
+
+export type RemoveChatMutation = { __typename?: 'Mutation' } & Pick<
+  Mutation,
+  'removeChat'
+>;
+
 export type GetChatQueryVariables = {
   chatId: Scalars['ID'];
 };
@@ -83,6 +113,12 @@ export type AddMessageMutation = { __typename?: 'Mutation' } & {
   addMessage: Maybe<{ __typename?: 'Message' } & MessageFragment>;
 };
 
+export type UsersListQueryVariables = {};
+
+export type UsersListQuery = { __typename?: 'Query' } & {
+  users: Array<{ __typename?: 'User' } & UserFragment>;
+};
+
 export type ChatFragment = { __typename?: 'Chat' } & Pick<
   Chat,
   'id' | 'name' | 'picture'
@@ -97,11 +133,29 @@ export type MessageFragment = { __typename?: 'Message' } & Pick<
   'id' | 'createdAt' | 'content' | 'isMine'
 > & { chat: Maybe<{ __typename?: 'Chat' } & Pick<Chat, 'id'>> };
 
+export type UserFragment = { __typename?: 'User' } & Pick<
+  User,
+  'id' | 'name' | 'picture'
+>;
+
 export type ChatsQueryVariables = {};
 
 export type ChatsQuery = { __typename?: 'Query' } & {
   chats: Array<{ __typename?: 'Chat' } & ChatFragment>;
 };
+
+export type ChatAddedSubscriptionVariables = {};
+
+export type ChatAddedSubscription = { __typename?: 'Subscription' } & {
+  chatAdded: { __typename?: 'Chat' } & ChatFragment;
+};
+
+export type ChatRemovedSubscriptionVariables = {};
+
+export type ChatRemovedSubscription = { __typename?: 'Subscription' } & Pick<
+  Subscription,
+  'chatRemoved'
+>;
 
 export type MessageAddedSubscriptionVariables = {};
 
@@ -140,6 +194,58 @@ export const FullChatFragmentDoc = gql`
   ${ChatFragmentDoc}
   ${MessageFragmentDoc}
 `;
+export const UserFragmentDoc = gql`
+  fragment User on User {
+    id
+    name
+    picture
+  }
+`;
+export const AddChatDocument = gql`
+  mutation AddChat($recipientId: ID!) {
+    addChat(recipientId: $recipientId) {
+      ...Chat
+    }
+  }
+  ${ChatFragmentDoc}
+`;
+export type AddChatMutationFn = ReactApollo.MutationFn<
+  AddChatMutation,
+  AddChatMutationVariables
+>;
+
+export function useAddChatMutation(
+  baseOptions?: ReactApolloHooks.MutationHookOptions<
+    AddChatMutation,
+    AddChatMutationVariables
+  >,
+) {
+  return ReactApolloHooks.useMutation<
+    AddChatMutation,
+    AddChatMutationVariables
+  >(AddChatDocument, baseOptions);
+}
+export const RemoveChatDocument = gql`
+  mutation RemoveChat($chatId: ID!) {
+    removeChat(chatId: $chatId)
+  }
+`;
+export type RemoveChatMutationFn = ReactApollo.MutationFn<
+  RemoveChatMutation,
+  RemoveChatMutationVariables
+>;
+
+export function useRemoveChatMutation(
+  baseOptions?: ReactApolloHooks.MutationHookOptions<
+    RemoveChatMutation,
+    RemoveChatMutationVariables
+  >,
+) {
+  return ReactApolloHooks.useMutation<
+    RemoveChatMutation,
+    RemoveChatMutationVariables
+  >(RemoveChatDocument, baseOptions);
+}
 export const GetChatDocument = gql`
   query GetChat($chatId: ID!) {
     chat(chatId: $chatId) {
@@ -181,6 +287,23 @@ export function useAddMessageMutation(
     AddMessageMutationVariables
   >(AddMessageDocument, baseOptions);
 }
+export const UsersListDocument = gql`
+  query UsersList {
+    users {
+      ...User
+    }
+  }
+  ${UserFragmentDoc}
+`;
+
+export function useUsersListQuery(
+  baseOptions?: ReactApolloHooks.QueryHookOptions<UsersListQueryVariables>,
+) {
+  return ReactApolloHooks.useQuery<UsersListQuery, UsersListQueryVariables>(
+    UsersListDocument,
+    baseOptions,
+  );
+}
 export const ChatsDocument = gql`
   query Chats {
     chats {
@@ -197,6 +320,43 @@ export function useChatsQuery(
     ChatsDocument,
     baseOptions,
   );
+}
+export const ChatAddedDocument = gql`
+  subscription ChatAdded {
+    chatAdded {
+      ...Chat
+    }
+  }
+  ${ChatFragmentDoc}
+`;
+
+export function useChatAddedSubscription(
+  baseOptions?: ReactApolloHooks.SubscriptionHookOptions<
+    ChatAddedSubscription,
+    ChatAddedSubscriptionVariables
+  >,
+) {
+  return ReactApolloHooks.useSubscription<
+    ChatAddedSubscription,
+    ChatAddedSubscriptionVariables
+  >(ChatAddedDocument, baseOptions);
+}
+export const ChatRemovedDocument = gql`
+  subscription ChatRemoved {
+    chatRemoved
+  }
+`;
+
+export function useChatRemovedSubscription(
+  baseOptions?: ReactApolloHooks.SubscriptionHookOptions<
+    ChatRemovedSubscription,
+    ChatRemovedSubscriptionVariables
+  >,
+) {
+  return ReactApolloHooks.useSubscription<
+    ChatRemovedSubscription,
+    ChatRemovedSubscriptionVariables
+  >(ChatRemovedDocument, baseOptions);
 }
 export const MessageAddedDocument = gql`
   subscription MessageAdded {
